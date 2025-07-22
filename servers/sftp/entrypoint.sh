86423035a32965798e97fe308f4897b30a51f137
Alexander Adams <alex.michael.adams@gmail.com>
2025-07-22 19:05:15 +1000

    Fix sidecar sftp entrypoint bug

--- servers/sftp/entrypoint.sh
+++ servers/sftp/entrypoint.sh
@@ -11,7 +11,7 @@ if [ ! -f /etc/ssh/ssh_host_ed25519_key ]; then
   ssh-keygen -A
 fi
 
-mkdir /root/.ssh
+mkdir -p /root/.ssh
 echo "$PUBLIC_KEY" > /root/.ssh/authorized_keys
 
 chmod 700 /root/.ssh
