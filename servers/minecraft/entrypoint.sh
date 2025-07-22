eb4c4ef18cb60cd3b5a723f08507a28ef4ae9ad8
Alexander Adams <alex.michael.adams@gmail.com>
2025-07-22 19:06:12 +1000

    Update workload minecraft Dockerfile and entrypoint

--- servers/minecraft/entrypoint.sh
+++ servers/minecraft/entrypoint.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/bash
 
-set -x
-set -e
+if [ -f "/data/run.sh" ]; then
+    exec /data/run.sh
+fi
 
 MANIFEST_URL="https://piston-meta.mojang.com/mc/game/version_manifest_v2.json"
-# ACCEPT_EULA=true
+MANIFEST=$(curl -s $MANIFEST_URL)
+
+VERSION="latest"
+if [[ "$PANEL_VERSION" == "latest" ]]; then
+    VERSION=$(echo $MANIFEST | jq -r '.latest.release')
+else
+    VERSION=$PANEL_VERSION
+fi
 
 if [ ! -f server.jar ]; then
-    MANIFEST=$(curl -s $MANIFEST_URL)
-    VERSION_LATEST=$(echo $MANIFEST | jq -r '.latest.release')
-    VERSION_LATEST_MANIFEST_URL=$(echo $MANIFEST | jq -r --arg version "${VERSION_LATEST}" '.versions[] | select(.id==$version) | .url')
+    VERSION_LATEST_MANIFEST_URL=$(echo $MANIFEST | jq -r --arg version "${VERSION}" '.versions[] | select(.id==$version) | .url')
     VERSION_LATEST_MANIFEST=$(curl -s $VERSION_LATEST_MANIFEST_URL)
     VERSION_LATEST_SERVER_URL=$(echo $VERSION_LATEST_MANIFEST | jq -r '.downloads.server.url')
     wget -O server.jar $VERSION_LATEST_SERVER_URL
@@ -19,7 +25,7 @@ if [[ ! -f eula.txt || ! -f server.properties ]]; then
     java -Xmx1024M -Xms1024M -jar server.jar --nogui --initSettings || exit 1
 fi
 
-if [ $ACCEPT_EULA = true ]; then
+if [ $PANEL_ACCEPT_EULA = true ]; then
     sed -i 's/^eula=false$/eula=true/' eula.txt
 fi
 
