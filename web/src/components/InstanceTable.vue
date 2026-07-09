8cbcc6751c908a166c25668519cc22ae2714e487
Alex Adams <alex.michael.adams@gmail.com>
2026-07-10 00:07:09 +1000

    Fix iPhone layout

--- web/src/components/InstanceTable.vue
+++ web/src/components/InstanceTable.vue
@@ -9,22 +9,24 @@ defineProps({
 </script>
 
 <template>
-  <table class="table table-hover clickable">
-    <thead>
-      <tr>
-        <th scope="col">Name</th>
-        <th scope="col">Image</th>
-        <th scope="col">Status</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="item in instances" :key="item.id" v-on:click="router.push(`/instances/${item.id}`)">
-        <td>{{ item.name }}</td>
-        <td>{{ item.image }}</td>
-        <td>{{ item.status }}</td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="table-responsive">
+    <table class="table table-hover clickable">
+      <thead>
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Image</th>
+          <th scope="col">Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in instances" :key="item.id" v-on:click="router.push(`/instances/${item.id}`)">
+          <td>{{ item.name }}</td>
+          <td>{{ item.image }}</td>
+          <td>{{ item.status }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <style scoped>
