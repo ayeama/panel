a9a23a1550babc0d5194ab7ef451069623cca9d2
Alex Adams <alex.michael.adams@gmail.com>
2026-09-25 10:03:25 +1000

    Support changing theme

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,9 +1,24 @@
 <script setup>
+import { computed } from 'vue'
 import { RouterView } from 'vue-router'
 
-import { useTheme } from './composables/useTheme'
+import { Theme, useTheme } from './composables/useTheme'
+import SunFill from './components/icons/SunFill.vue'
+import MoonFill from './components/icons/MoonFill.vue'
+import CircleHalfFill from './components/icons/CircleHalfFill.vue'
 
-useTheme()
+const { getTheme, setTheme } = useTheme()
+
+const themeIcon = computed(() => {
+  switch (getTheme()) {
+    case Theme.LIGHT:
+      return SunFill
+    case Theme.DARK:
+      return MoonFill
+    default:
+      return CircleHalfFill
+  }
+})
 </script>
 
 <template>
@@ -11,6 +26,41 @@ useTheme()
     <nav class="navbar bg-body-tertiary">
       <div class="container">
         <RouterLink class="navbar-brand" to="/">Panel</RouterLink>
+
+        <div class="dropdown">
+          <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown">
+            <component :is="themeIcon" />
+          </button>
+          <ul class="dropdown-menu dropdown-menu-end">
+            <li>
+              <a
+                class="dropdown-item"
+                :class="{ active: getTheme() === Theme.LIGHT }"
+                href="#"
+                @click.prevent="setTheme(Theme.LIGHT)"
+                ><SunFill /> Light</a
+              >
+            </li>
+            <li>
+              <a
+                class="dropdown-item"
+                :class="{ active: getTheme() === Theme.DARK }"
+                href="#"
+                @click.prevent="setTheme(Theme.DARK)"
+                ><MoonFill /> Dark</a
+              >
+            </li>
+            <li>
+              <a
+                class="dropdown-item"
+                :class="{ active: getTheme() === Theme.SYSTEM }"
+                href="#"
+                @click.prevent="setTheme(Theme.SYSTEM)"
+                ><CircleHalfFill /> Auto</a
+              >
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   </header>
--- web/src/composables/useTheme.js
+++ web/src/composables/useTheme.js
@@ -1,19 +1,47 @@
-import { onMounted, onUnmounted } from 'vue'
+import { ref, onMounted, onUnmounted } from 'vue'
 
-function setTheme(dark) {
-  document.documentElement.setAttribute('data-bs-theme', dark ? 'dark' : 'light')
-}
+export const Theme = Object.freeze({
+  LIGHT: 'light',
+  DARK: 'dark',
+  SYSTEM: 'system',
+})
 
 export function useTheme() {
+  const theme = ref(Theme.SYSTEM)
+
+  function getTheme() {
+    return theme.value
+  }
+
+  function setTheme(value) {
+    theme.value = value
+    applyTheme()
+  }
+
   const media = window.matchMedia('(prefers-color-scheme: dark)')
-  const handleTheme = (e) => setTheme(e.matches)
+
+  function applyTheme() {
+    const dark = theme.value == Theme.DARK || (theme.value == Theme.SYSTEM && media.matches)
+    document.documentElement.setAttribute('data-bs-theme', dark ? Theme.DARK : Theme.LIGHT)
+  }
+
+  function handleTheme() {
+    if (theme.value == Theme.SYSTEM) {
+      applyTheme()
+    }
+  }
 
   onMounted(() => {
-    setTheme(media.matches)
+    applyTheme()
     media.addEventListener('change', handleTheme)
   })
 
   onUnmounted(() => {
     media.removeEventListener('change', handleTheme)
   })
+
+  return {
+    getTheme,
+    setTheme,
+  }
 }
